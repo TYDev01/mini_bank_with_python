6153c6ca70af3f217163288422d5df620702658d
Anthony <anthonychinaemeru84@yahoo.com>
2024-03-31 10:49:28 +0100

    Added the balance function

---
+++ bank.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self, initial_amount, account_name):
+        self.initial_amount = initial_amount
+        self.account_name = account_name
+
+    def __str__(self):
+        return f"Account: '{self.account_name}' created.\nBalance = ${self.initial_amount:,.2f}"
+    
+    def getBalance(self):
+        return f"{self.account_name} account balance is ${self.initial_amount:,.2f}"
+    
+    
